aad1609a53c491b809dda2a379964a603688ce51
Sofie <sozp@itu.dk>
2026-03-25 11:16:44 +0100

    changed feature file A and B

---
+++ src/feature_A.py
@@ -0,0 +1,9 @@
+def get_asymmetry(mask):
+    # mask = color.rgb2gray(mask)
+    scores = []
+    for _ in range(6):
+        segment = crop(mask)
+        (np.sum(segment))
+        scores.append(np.sum(np.logical_xor(segment, np.flip(segment))) / (np.sum(segment)))
+        mask = rotate(mask, 30)
+    return sum(scores) / len(scores)
---
+++ src/feature_B.py
@@ -0,0 +1,9 @@
+def get_compactness(mask):
+    # mask = color.rgb2gray(mask)
+    area = np.sum(mask)
+
+    struct_el = morphology.disk(3)
+    mask_eroded = morphology.binary_erosion(mask, struct_el)
+    perimeter = np.sum(mask - mask_eroded)
+
+    return perimeter**2 / (4 * np.pi * area)
