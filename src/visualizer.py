021d73ad2255dd85303539302b4e9227d808eb89
Hunor Szabo <szhuni1234@gmail.com>
2026-03-31 12:23:51 +0200

    initial visualiser file added. Needs changes though!

---
+++ src/visualizer.py
@@ -0,0 +1,23 @@
+import pipeline_own
+
+
+
+
+
+# plt.figure(figsize=(15,5))
+# plt.subplot(1,3,1)
+# plt.imshow(img1.img_org)
+# plt.title('Original')
+# plt.axis('off')
+
+# plt.subplot(1,3,2)
+# plt.imshow(img1.hair_mask, cmap='gray')
+# plt.title('Hair Mask')
+# plt.axis('off')
+
+# plt.subplot(1,3,3)
+# plt.imshow(img1.hairless)
+# plt.title('Hair Removed')
+# plt.axis('off')
+
+# plt.show()
